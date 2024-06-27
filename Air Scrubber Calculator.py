1cacf66c917ea78c95351a5b4586e6b1b075fd95
Jkavadias21 <johnnykavadias@gmail.com>
2024-06-27 22:50:17 +1000

    seemingly functional combination calculator with string representation

--- Air Scrubber Calculator.py
+++ Air Scrubber Calculator.py
@@ -1,9 +1,9 @@
-
 class airScrubber:
     # Constructor method (initializer)
     def __init__(self, scrubberType, cfmValue):
         self.scrubberType = scrubberType
         self.cfmValue = cfmValue
+        
 
     def setFlag(self, flag):
         self.flag = flag
@@ -25,73 +25,84 @@ def findScrubberCombos(scrubbersCfms, cfmTar):
     backtrack(0, [], 0)
     return result
 
-#def print_sums(array_of_arrays):
-    for sub_array in array_of_arrays:
-        array_sum = sum(sub_array)
-        print(f"Sum of {sub_array} is {array_sum}")
-
-
-##call filtering function
-def findCloseCombos(scrubberCombos, cfmTarget, original):
+#filter out undesired combos
+def filterAirScrubbers(scrubberCombos, cfmTarget, original):
     finalArray = []
     toRemove = []
-    list = scrubberCombos.copy()
-    i = 1
     
-    #removes combo from original array that have a cfm sum equal to the target cfm
     for scrubberCombo in scrubberCombos:
+        #add combo from original array that have a cfm sum equal to the target cfm to a removal list and a final list
         if sum(scrubberCombo) == cfmTarget:
             finalArray.append(scrubberCombo)
             toRemove.append(scrubberCombo)
+        #add combo thats more than one AS away from target to removal list
         if cfmTarget - sum(scrubberCombo) >= max(original):
             toRemove.append(scrubberCombo)
-
+    
+    #remove undesired combos
     for items in toRemove:
         scrubberCombos.remove(items)
-    print (scrubberCombos, "-------------")
+
+    #--------------testing ----------------
+    toRemove = list(filter(None, toRemove))
+    scrubberCombos = list(filter(None, scrubberCombos))
+    print("removed      ", toRemove)
+    print("after removal", scrubberCombos)
+    #--------------testing ----------------
+    
+    #return array with cfm sums equal to target cfm
+    return finalArray
+
+#encode overflow scrubbers into combos
+def addOverflowScrubber(scrubberCombos, original):
+    i = 1
     for scrubberCombo in scrubberCombos:
-        
         scrubberCombo.append("")
         integers = [x for x in scrubberCombo if isinstance(x, int)]
 
-        print("this line", scrubberCombo, integers)
-        
-        
+        #represent overflow scrubbers as strings in the final slot of combo array in a (AS1/AS2) format
         for cfm in original:
-            #print(i, len(original), "length")
-            if (sum(integers) + cfm > cfmTarget) and (i < len(original)):
-                print("first", i, len(original))
-                scrubberCombo[-1] = scrubberCombo[-1] + str(cfm) + ' or '
-            elif (sum(integers) + cfm > cfmTarget):
-                print("second", i, len(original), sum(integers) + cfm, cfmTarget)
-                scrubberCombo[-1] = scrubberCombo[-1] + str(cfm)
-            i += 1
-        i = 1
-    print(toRemove, "removed") 
-    print(list, "list")
-    print(original, "og")       
-    print(scrubberCombos, "after removal")
-    return finalArray
+            for scrubber in scrubbers:
+                if cfm == scrubber.cfmValue:
+                    tempCfm = scrubber.scrubberType
             
-        
-        
-        
+            if (sum(integers) + cfm > cfmTarget) and (i < len(original)):
+                scrubberCombo[-1] = scrubberCombo[-1] + str(tempCfm) + '/'
+            elif (sum(integers) + cfm > cfmTarget):
+                scrubberCombo[-1] = scrubberCombo[-1] + str(tempCfm)
+            i += 1
+        i = 1 
 
-scrubbersCfms = [2,3,4]
-cfmTarget = 7
+scrubbersCfms = []
+
+#list to store all scrubbers the user has at their facility
+scrubbers = [airScrubber("pheonix", 485), airScrubber("xPower", 650), airScrubber("thor", 1000)]
+#list that stores all the users air scrubbers cfm values 
+for scrubber in scrubbers:
+    scrubbersCfms.append(scrubber.cfmValue)
+
+
+cfmTarget = 1000
 scrubberCombos = findScrubberCombos(scrubbersCfms, cfmTarget)
-print(scrubberCombos, "original")
-final = findCloseCombos(scrubberCombos, cfmTarget, scrubbersCfms)
-#print(print_sums(scrubberCombos))
+
+#-----testing------
+print( "original     ", scrubberCombos)
+
+final = filterAirScrubbers(scrubberCombos, cfmTarget, scrubbersCfms)
+addOverflowScrubber(scrubberCombos, scrubbersCfms)
 final = final + scrubberCombos
-print("\n", scrubberCombos, "original")
-print("\n\n", final, "final")
 
-##calculates all combinations less than or equal to target just need to filter out and print
+print("final        ", final)
 
-#just keep elements that with one addition of any of the cfm values exeed the target(filter out 2 step remaining ones)
-# if combo is subset of any other combo get rid of it
+#convert cfm values in combo lists to air scrubber name strings
+for i in range(len(final)):
+    for j in range(len(final[i])):
+        for scrubber in scrubbers:
+            if final[i][j] == scrubber.cfmValue:
+                final[i][j] = scrubber.scrubberType
+final = sorted(final, key=len)
 
-# take combo add each cfm to its sum if any of them exeed the target then add that combo example [3] with a goal of 5 if i add 1 (3+1) = 4 < 5 dont add since already registered combo 
-#if i add 2 (3+2) = 5 = 5 dont add since this is already a registered combo
-#if i ad 3 (3+3) = 6 > 5 add this since it is only 1 step away and exeeds target
+#-----testing------
+for combo in final:
+    print(combo)
+
